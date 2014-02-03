9ed5d0cdca0cb240b164858b2b05c1809d49efef
SoftDrink <SamuelD@10.176.254.237>
2014-02-03 16:02:27 +0100

    Adding body to DatabaseHandler

--- src/DataLayer/DatabaseHandler.java
+++ src/DataLayer/DatabaseHandler.java
@@ -3,10 +3,91 @@
  */
 package DataLayer;
 
+import java.sql.*;
+
 /**
  * @author SamuelD
  *
  */
 public class DatabaseHandler {
+	private String connectionString;
+	private String user;
+	private String pass;
+	private Connection connection;
+	
+	public DatabaseHandler() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Driver --- Loaded ---");
+			/**
+			 * TODO : Add db name
+			 */
+			this.connectionString = "jdbc:mysql://localhost/";
+			this.user = "root";
+			this.pass = "";
+			
+			this.connection = DriverManager.getConnection(this.connectionString, this.user, this.pass);
+			
+			System.out.println("Connection --- Loaded ---");
+		} catch (Exception e) {
+			System.out.println("Database --- Fail loading or connecting ---");
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * @define Method which return a selected ResultSet of rows
+	 * @param state : Statement
+	 * @return result : ResultSet
+	 */
+	public ResultSet getRows(String query) {
+		ResultSet result = null;
+		try {
+			Statement state = this.connection.createStatement();
+			result = state.executeQuery(query);
+		} catch (Exception e) {
+			System.out.println("--- Fail querying --- Query = " + query);
+			e.printStackTrace();
+		}
+		return result;
+	}
+	/**
+	 * @define Method which insert or update a row in a database
+	 * @param query : String
+	 */
+	public void setRows(String query) {
+		try {
+			Statement state = this.connection.createStatement();
+			state.executeUpdate(query);
+		} catch (Exception e) {
+			System.out.println("--- Fail Seting rows --- Query = " + query);
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * @return the user
+	 */
+	public String getUser() {
+		return user;
+	}
 
+	/**
+	 * @param user the user to set
+	 */
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	/**
+	 * @return the pass
+	 */
+	public String getPass() {
+		return pass;
+	}
+
+	/**
+	 * @param pass the pass to set
+	 */
+	public void setPass(String pass) {
+		this.pass = pass;
+	}	
 }
